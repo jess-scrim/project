3215b32731a1b0bf34b5ce144c9cdc9e04e2d366
jess-scrim <jessica.scrimshire@gmail.com>
2023-11-02 18:06:45 +0000

    updated plot with wider axis, added updated bigram-plots

--- scripts/04_publication_date.R
+++ scripts/04_publication_date.R
@@ -2,7 +2,10 @@
 
 abstracts %>%  
   ggplot(aes(date)) +
-  geom_histogram(bins = 100) +
+  geom_histogram(bins = 50) +
   xlab("Date of Publication") +
-  ylab("Number of Abstracts") 
-  
+  ylab("Number of Abstracts") +
+  # 10 month intervals
+  scale_x_date(date_breaks = "1 month", date_labels = "%m/%Y") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) 
+scale_y_continuous(breaks = seq(0, 1400, by = 200))  
--- scripts/pre_post_drug.R
+++ scripts/pre_post_drug.R
@@ -1,11 +1,12 @@
 # Filter data for pre and post drug 
 
 lena <- c("lenacenmab", 
-          "Leqembi®",
+          "Leqembi",
           "DB14580",
           "BAN2401",
           "lecanemab-irmb",
-          "mAb158")
+          "mAb158",
+          "TRAILBLAZER-ALZ")
 
 dona <- c("donanemab",
           "LY3002813",
@@ -13,4 +14,16 @@ dona <- c("donanemab",
 
 rema <- C("remternetug",
           "LY3372993")
+
+tidy_abstracts_clean %>% 
+  filter(word %in% tolower(lena)) %>% View()
+  
+### See if there is a change in the literature before and after the drug was announced ###
+
+pre_lena <- tidy_abstracts_clean %>% 
+  filter(date <= "2023-01-06")  # 3,437 abstracts
+post_lena <- tidy_abstracts_clean %>% 
+  filter(date > "2023-01-06")  
+
+post_lena %>% distinct(abstract) %>% count()
 
