33cfb08a20f6b7ce41e2d8fffd5797f2b9bdde11
jess-scrim <jessica.scrimshire@gmail.com>
2024-01-26 16:08:05 +0000

    updated Quarto report and added references

--- scripts/02_tokenisation.R
+++ scripts/02_tokenisation.R
@@ -3,18 +3,21 @@ source("scripts/00_setting_up.R")
 # Tokenise
 tidy_abstracts <- abstracts %>% 
   filter(!text == "NA") %>% 
-  mutate(type = case_when(date <= leca_approv ~ "pre_lena",
+  mutate(type = case_when(date <= leca_approv ~ "pre-lena",
                           date > leca_approv ~ "post-lena")) %>% 
   unnest_tokens(word, text)
 
 # Remove stopwords
 data(stop_words)
-my_stopwords <- tibble(word = c("alzheimer's", "ad"))
 
 tidy_abstracts_clean <- tidy_abstracts %>%
   anti_join(stop_words)
 tidy_abstracts_clean <- tidy_abstracts_clean %>%
   anti_join(my_stopwords)
+
+# save tidy_abstracts_clean to data file
+ tidy_abstracts_clean %>%
+  write_csv("results/tidy_abstracts_clean.csv")
 
 # Plot most common words among all abstracts
 tidy_abstracts_clean %>%  
